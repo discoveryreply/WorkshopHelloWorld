d91d1d760930e7e22841e7cc2b693692af8848de
Discovery <discovery.factory.mdw@reply.it>
2021-10-05 13:32:46 +0200

    first commit

---
+++ src/test/java/com/workshop/example/WorkshopHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
